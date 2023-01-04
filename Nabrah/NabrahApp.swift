2005d132ed8415ba39ee129d3edf0e0764cb8cc9
Mashael Alghunaim <mashael.alghunaim1@gmail.com>
2023-01-04 18:23:42 +0300

    Initial Commit

---
+++ Nabrah/NabrahApp.swift
@@ -0,0 +1,17 @@
+//
+//  NabrahApp.swift
+//  Nabrah
+//
+//  Created by Mashael Alghunaim on 11/06/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct NabrahApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
